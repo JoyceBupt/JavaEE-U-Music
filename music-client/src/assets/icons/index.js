da803e91744117c0e83bf21825aa037f4d7c28a9
Wang Yuming <109780629+JoyceBupt@users.noreply.github.com>
2023-09-07 03:02:58 +0800

    frontend-client-v1.4

---
+++ music-client/src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import './iconfont.js'
+import './iconfont1.js'
+import './iconfont2.js'
+import './iconfont3.js'
+import './iconfont4.js'
+
+// 使用多个<symbol>元素，每个<symbol>元素定义一个图标，其中包含路径（<path>）元素，路径元素定义图标的形状
